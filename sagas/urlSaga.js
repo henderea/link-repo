6ab9a870ddd844742a51c92550f2ac04eb839378
Eric Henderson <henderea@gmail.com>
2020-07-06 13:44:40 -0400

    take out the ky dependency to improve bundle size a bit

--- sagas/urlSaga.js
+++ sagas/urlSaga.js
@@ -2,7 +2,6 @@ import { all, put, takeLatest, select } from 'redux-saga/effects';
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/database';
-import ky from 'ky/umd';
 
 import { urlActionTypes as actionTypes, addUrlSuccess, addUrlError, removeUrlSuccess, removeUrlError } from '../actions/urlActions';
 import { selectUid } from '../selectors/authSelectors';
@@ -13,7 +12,7 @@ function* addUrlProcess() {
     const uid = yield select(selectUid());
     const typedUrl = yield select(selectTypedUrl());
     if(typedUrl.length > 0) {
-      const metadata = yield ky.get('/api/metascraper', { searchParams: { url: typedUrl } }).json();
+      const metadata = yield fetch(`/api/metascraper?url=${encodeURIComponent(typedUrl)}`, { method: 'GET' }).then(res => res.json());
       const {description, title, url, image, logo} = metadata;
       yield firebase.database().ref(`urls/${uid}`).push({
         description,
